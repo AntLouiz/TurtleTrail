78e79f5415987976e2be58aacc8368d75d31f09a
Gonzaga <luiz.oliveira@mercos.com>
2023-12-23 16:18:49 -0300

    Add basic tokens

---
+++ src/tokens/colors.js
@@ -0,0 +1,8 @@
+export const COLORS = {
+    primary: '#6750A4',
+    secondary: '#2ecc71',
+    background: '#F3EDF7',
+    text: 'white',
+    accent: '#e74c3c',
+    transparent: 'transparent'
+}
