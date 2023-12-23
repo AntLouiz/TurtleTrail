721d6a80812427b0333ff985a10a0068edb6f22b
Gonzaga <luiz.oliveira@mercos.com>
2023-12-23 16:20:59 -0300

    Add ModalAddTurtle component

---
+++ src/pages/TurtlesList/ModalAddTurtle.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { Text, View, StyleSheet } from "react-native"
+
+import Modal from '../../components/Modal/Modal'
+
+
+type Props = {
+  visible: boolean;
+  onClose: () => void;
+};
+
+export const ModalAddTurtle = (props: Props) => {
+  return (
+    <Modal title="Add a turtle" visible={props.visible} onClose={props.onClose}>
+      <View>
+        <Text>Form here</Text>
+      </View>
+      <Modal.Footer>
+        <Modal.CancelButton title="Cancel" onPress={props.onClose} />
+        <Modal.ConfirmButton title="Create" onPress={() => {}} />
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+});
