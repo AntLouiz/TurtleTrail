031faa0a8c572b66e535bbb7a1418b2af3c6789d
Gonzaga <luiz.oliveira@mercos.com>
2023-12-23 16:22:29 -0300

    Fix App eslint

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import "react-native-gesture-handler";
+import React from 'react'
+
+import 'react-native-gesture-handler'
 
 import Navigator from "./src/Navigator";
 
 export default function App() {
-  return <Navigator />;
+  return <Navigator />
 }
