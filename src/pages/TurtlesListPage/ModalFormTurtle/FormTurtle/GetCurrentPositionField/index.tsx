85f7fb5efebf0dafa7964ac70bd2f3eaab4fb9a6
Gonzaga <luiz.oliveira@mercos.com>
2024-05-18 15:26:16 -0300

    Move o componente que obtem a localização

---
+++ src/pages/TurtlesListPage/ModalFormTurtle/FormTurtle/GetCurrentPositionField/index.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+
+import { Text } from 'react-native'
+
+import Geolocation from '@react-native-community/geolocation'
+import { useController, useFormContext } from 'react-hook-form'
+
+import { Button } from '../../../../../components/Button'
+import { Location } from '../../../../../types'
+
+
+export const CurrentPositionField = (props: { name: string }) => {
+    const [isGettingLocation, setIsGettingLocation] = useState(false)
+    const { control } = useFormContext()
+    const { field } = useController({ control, name: props.name })
+
+    const location: Location | undefined = field.value
+    const buttonText = isGettingLocation ? 'Getting location...' : 'Get actual location'
+    const labelText = location ?
+        `Lat: ${location?.latitude}, Long: ${location?.longitude}`:
+        'No location assigned'
+
+    const disable = () => setIsGettingLocation(true)
+    const enable = () => setIsGettingLocation(false)
+
+    return (
+        <>
+            <Text>{labelText}</Text>
+            <Button
+                type='outline'
+                title={buttonText}
+                disabled={isGettingLocation}
+                onPress={() => {
+                    disable()
+                    Geolocation.getCurrentPosition(info => {
+                        const { latitude, longitude } = info.coords
+                        field.onChange({ latitude, longitude })
+                        enable()
+                    })
+                }}
+            />
+        </>
+    )
+}
--- src/pages/TurtlesListPage/ModalFormTurtle/FormTurtle/index.tsx
+++ src/pages/TurtlesListPage/ModalFormTurtle/FormTurtle/index.tsx
@@ -1,15 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-import { View, StyleSheet, Text } from 'react-native'
-import { useForm, FormProvider, FieldValues, useFormContext, useController } from 'react-hook-form'
-import Geolocation from '@react-native-community/geolocation'
+import { View, StyleSheet } from 'react-native'
+import { useForm, FormProvider, FieldValues } from 'react-hook-form'
 
 import { TextField } from '../../../../components/TextField'
 import Modal from '../../../../components/Modal'
-import { Button } from '../../../../components/Button'
+import { CurrentPositionField } from './GetCurrentPositionField'
 import { SPACING } from '../../../../tokens/spacing'
 
-import { Turtle, Location } from '../../../../types'
+import { Turtle } from '../../../../types'
 
 type Props = {
     defaultValues: Turtle | null
@@ -41,42 +40,6 @@ export const FormTurtle = (props: Props) => {
                 </Modal.Footer>
             </View>
         </FormProvider>
-    )
-}
-
-const CurrentPositionField = (props: { name: string }) => {
-    const [isGettingLocation, setIsGettingLocation] = useState(false)
-    const { control } = useFormContext()
-    const { field } = useController({ control, name: props.name })
-
-    const location: Location | undefined = field.value
-    const buttonText = isGettingLocation ? 'Getting location...' : 'Get actual location'
-    const valueText = location ?
-        `Lat: ${location?.latitude}, Long: ${location?.longitude}`:
-        'No location assigned'
-
-    const disable = () => setIsGettingLocation(true)
-    const enable = () => setIsGettingLocation(false)
-
-    return (
-        <>
-            <Text>
-                {valueText}
-            </Text>
-            <Button
-                type='outline'
-                title={buttonText}
-                disabled={isGettingLocation}
-                onPress={() => {
-                    disable()
-                    Geolocation.getCurrentPosition(info => {
-                        const { latitude, longitude } = info.coords
-                        field.onChange({ latitude, longitude })
-                        enable()
-                    })
-                }}
-            />
-        </>
     )
 }
 
